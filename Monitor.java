55fb2cd99ff315824e128820d19afadcf4af48d8
markrp <markrepedersen@gmail.com>
2020-01-26 23:39:04 -0800

    Implemented Responder and some of Monitor.

--- Monitor.java
+++ Monitor.java
@@ -1,84 +1,198 @@
-import java.util.concurrent.LinkedBlockingQueue;
-import java.net.InetAddress;
+import java.net.DatagramPacket;
 import java.net.DatagramSocket;
+import java.net.InetAddress;
 import java.net.SocketException;
+import java.net.SocketTimeoutException;
+import java.nio.ByteBuffer;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.concurrent.LinkedBlockingQueue;
 
-public class Monitor {
-    // The reserved nonce value;
-    public static final long RESERVED_NONCE = -1;
+public class Monitor extends Runnable {
+  // The reserved nonce value;
+  public static final long RESERVED_NONCE = -1;
 
+  private static final int BUFFER_SIZE_IN_BYTES = 16;
+  private static boolean isMonitoringAll = true;
 
-    // The shared "channel" that all monitors use to indicate that a failure has
-    // been detected
-    static LinkedBlockingQueue<Monitor> clq = new LinkedBlockingQueue<Monitor>();
+  // The shared "channel" that all monitors use to indicate that a failure has
+  // been detected
+  static LinkedBlockingQueue<Monitor> clq = new LinkedBlockingQueue<Monitor>();
 
-    // This variable is to be used to hold a string that unqiquelly identifies the Monitor
-    String name;
-    
-    static long eNonce = RESERVED_NONCE;
-    int threshHold;
+  // This variable is to be used to hold a string that unqiquelly identifies the
+  // Monitor
+  String name;
 
-    // This class method is used to set the epoch nonce, which is the value of the
-    // epochNonce parameter. The return value is the channel the caller can
-    // monitor to be notified that a failure has been detected.  If this method
-    // is called more than once then a FailureDetectorException with the msg
-    // "Monitor:  Already Initialized" is to be thrown.  The epoch value of -1 is
-    // reserved and cannot be used. If the epochNonce is -1 then a
-    // FailureDetectorException with the msg "Monitor: Invalid Epoch" is to be thrown.
-    public static LinkedBlockingQueue<Monitor> initializeMonitor(long epochNonce)
-	throws FailureDetectorException {
-	System.out.println("initializeMonitor needs to be implemented");
-	return clq;
+  private DatagramSocket socket;
+  private int currentRTT;
+  private List<Integer> roundTripTimes;
+  private int numHeartbeats;
+  private int lastAckReceivedInMs;
+  private int lastAckSentInMs;
+  private int lostMessageCount;
+  private int sequenceNumber;
+  private int lport;
+  private InetAddress laddr;
+  private int rport;
+  private InetAddress raddr;
+  private boolean isMonitoring;
+
+  static long eNonce = RESERVED_NONCE;
+  int threshHold;
+
+  // This class method is used to set the epoch nonce, which is the value of the
+  // epochNonce parameter. The return value is the channel the caller can
+  // monitor to be notified that a failure has been detected. If this method
+  // is called more than once then a FailureDetectorException with the msg
+  // "Monitor:  Already Initialized" is to be thrown. The epoch value of -1 is
+  // reserved and cannot be used. If the epochNonce is -1 then a
+  // FailureDetectorException with the msg "Monitor: Invalid Epoch" is to be
+  // thrown.
+  public static LinkedBlockingQueue<Monitor> initializeMonitor(long epochNonce) throws FailureDetectorException {
+    if (epochNonce == RESERVED_NONCE) {
+      throw new FailureDetectorException("Monitor: Invalid Epoch");
     }
+    eNonce = epochNonce;
+    return clq;
+  }
 
-    // This class method is to cause all Monitors to stop monitoring their remote
-    // node. 
-    public static void stopMonitoringAll() {
-     	System.out.println("stopMonitoringAll needs to be implemented");
+  // This class method is to cause all Monitors to stop monitoring their remote
+  // node.
+  public static void stopMonitoringAll() {
+    isMonitoringAll = false;
+  }
+
+  // This constructor sets up a Monitor, but it does not start sending heartbeat
+  // messages until the startMonitoring method is invoked.
+  // laddr - the address of the local interface to send UDP packets from and
+  // receive UDP packets on
+  // lport - the local port to use when sending heartbeat messages and receiving
+  // the
+  // acks
+  // raddr - the remote IP address to send the heartbeat messages to
+  // rport - the remote port number to sent the heartbeat messages to
+  // u_name - This sting is to be human readable value that the caller can
+  // use to provide a printable name for this Monitor.
+  //
+  // If the laddr:lport combination identify a port that cannot be opened
+  // for the sending of UDP packets then a SocketException is to be thrown.
+  // If any other type of error is detected that would prevent the operation of
+  // the monitor then a FailureDetectionException, with a useful name (i.e.
+  // the String parameter) is to be thrown.
+  public Monitor(InetAddress laddr, int lport, InetAddress raddr, int port, String u_name)
+      throws FailureDetectorException, SocketException {
+    this.name = u_name;
+    this.laddr = laddr;
+    this.lport = lport;
+    this.raddr = raddr;
+    this.rport = port;
+    this.isMonitoring = false;
+    this.sequenceNumber = 0;
+    this.lostMessageCount = 0;
+    this.numHeartbeats = 0;
+    this.roundTripTimes = new ArrayList<>();
+
+    if (!this.isPortAvailable(lport, laddr)) {
+      throw new SocketException("Port on this interface is already in use.");
     }
-    
-    // This constructor sets up a Monitor, but it does not start sending heartbeat
-    // messages until the startMonitoring method is invoked.
-    //  laddr - the address of the local interface to send UDP packets from and
-    //          receive UDP packets on
-    //  lport - the local port to use when sending heartbeat messages and receiving the
-    //          acks
-    //  raddr - the remote IP address to send the heartbeat messages to
-    //  rport - the remote port number to sent the heartbeat messages to
-    //  u_name - This sting is to be human readable value that the caller can
-    //           use to provide a printable name for this Monitor.
-    //
-    // If the laddr:lport combination identify a port that cannot be opened
-    // for the sending of UDP packets then a SocketException is to be thrown.
-    // If any other type of error is detected that would prevent the operation of
-    // the monitor then a FailureDetectionException, with a useful name (i.e.
-    // the String parameter) is to be thrown.
-    public Monitor(InetAddress laddr, int lport, InetAddress raddr, int port,
-		   String u_name)
-	throws FailureDetectorException, SocketException {
-	name = u_name;
-	System.out.println("The monitor constructor needs to be implemented");
+  }
+
+  private void setSocketTimeout() {
+    int rtt = this.estimateRTT();
+    socket.setSoTimeout(rtt);
+  }
+
+  private void waitForAck() {
+    while (this.isMonitoring) {
+      try {
+        DatagramPacket response = this.createPacket(raddr, rport);
+        this.setSocketTimeout();
+        socket.receive(response);
+
+        ByteBuffer data = ByteBuffer.wrap(response.getData());
+
+        if (data.capacity() == BUFFER_SIZE_IN_BYTES) {
+          long epoch = data.getLong();
+          long sequenceNumber = data.getLong();
+
+          if (epoch != eNonce || sequenceNumber != this.sequenceNumber) {
+            continue;
+          }
+          this.lastAckReceivedInMs = System.currentTimeMillis();
+        }
+      } catch (SocketTimeoutException exception) {
+        this.lostMessageCount++;
+        break;
+      }
     }
+  }
 
-    // Start (or restart) monitoring the remote node using the threshold value.
-    // If monitroing is currently in progress then the threshold value is
-    // set to this new value. Note: this call does not block.
-    public void startMonitoring(int threshold) throws FailureDetectorException {
-	System.out.println("startMonitoring needs to be implemented");
+  @Override
+  public void run() {
+    if (!this.isInitialized()) {
+      throw new FailureDetectorException("Not initialized.");
     }
-
-    // Stop monitoring the remote node. If the monitoring is currently in progress
-    // then nothing is done. Any ACK messages received after this message are to be
-    // ignored.
-    public void stopMonitoring() {
-	System.out.println("stopMonitoring needs to be implemented");
+    while (this.isMonitoring && this.lostMessageCount <= (this.threshHold - 1)) {
+      this.socket = new DatagramSocket(lport, laddr);
+      this.sendHeartbeat();
+      this.waitForAck();
     }
+  }
 
-    // Return the user supplied name for this Monitor
-    public String getName() {
-	return name;
+  private double estimateRTT() {
+    if (this.numHeartbeats <= 1) {
+      return 3000;
     }
+    this.roundTripTimes.add(this.lastAckReceivedInMs - this.lastAckSentInMs);
+    var stats = roundTripTimes.stream().mapToInt(Integer::intValue).summaryStatistics();
+    return stats.getAverage();
+  }
 
+  private DatagramPacket createPacket(InetAddress address, int port) {
+    ByteBuffer buffer = ByteBuffer.allocate(BUFFER_SIZE_IN_BYTES);
+    return new DatagramPacket(buffer.array(), buffer.capacity(), address, port);
+  }
 
+  private DatagramPacket createPacket(InetAddress address, int port, ByteBuffer buffer) {
+    return new DatagramPacket(buffer.array(), buffer.capacity(), address, port);
+  }
+
+  private void sendHeartbeat() {
+    ByteBuffer buffer = ByteBuffer.allocate(BUFFER_SIZE_IN_BYTES);
+    buffer.putLong(eNonce);
+    buffer.putLong(this.sequenceNumber);
+
+    DatagramPacket packet = this.createPacket(raddr, rport, buffer);
+
+    this.numHeartbeats++;
+    this.lastAckSentInMs = System.currentTimeMillis();
+    socket.send(packet);
+    this.sequenceNumber++;
+  }
+
+  // Start (or restart) monitoring the remote node using the threshold value.
+  // If monitroing is currently in progress then the threshold value is
+  // set to this new value. Note: this call does not block.
+  public void startMonitoring(int threshold) throws FailureDetectorException {
+    this.isMonitoring = true;
+    this.threshHold = threshold;
+    Thread thread = new Thread(this);
+    thread.start();
+  }
+
+  // Stop monitoring the remote node. If the monitoring is currently in progress
+  // then nothing is done. Any ACK messages received after this message are to be
+  // ignored.
+  public void stopMonitoring() {
+    this.isMonitoring = false;
+  }
+
+  // Return the user supplied name for this Monitor
+  public String getName() {
+    return name;
+  }
+
+  private boolean isInitialized() {
+    return eNonce == RESERVED_NONCE;
+  }
 }
-    
--- Responder.java
+++ Responder.java
@@ -1,39 +1,132 @@
-
-import java.net.InetAddress;
+import java.io.IOException;
+import java.net.DatagramPacket;
 import java.net.DatagramSocket;
+import java.net.InetAddress;
 import java.net.SocketException;
+import java.nio.ByteBuffer;
+import java.util.HashMap;
+import java.util.Map;
 
 // This class sets up and controls the part of the system that responds to heartbeat messages
 
-public class Responder {
+public class Responder extends Thread {
+  private static final int MESSAGE_SIZE_IN_BYTES = 16;
+  private static final Map<K, V> responders = new HashMap<InetAddress, Map<Integer, Boolean>>();
 
-    // This constructor creates a Responder object.
-    //    port -- the port to listen for heartbeat messages
-    //    laddr -- the IP address of the local interface to listen on. Keep in mind that a machine may
-    //             have multiple interfaces and this is a way to select which interface to listen on.
-    // If a  machine has multiple interfaces that require responders then multiple Responders can be used
-    // to achieve this functonality. If the local ip address/port combination is invalid or in use then
-    // a SocketException is to be thrown. Any other errors that result in a problem that would allow
-    // things not to be monitored will result in a FailureDetectorException being thrown. 
+  private DatagramSocket socket;
+  private int port;
+  private InetAddress addr;
+  private boolean isResponding;
 
+  // This constructor creates a Responder object.
+  // port -- the port to listen for heartbeat messages
+  // laddr -- the IP address of the local interface to listen on. Keep in mind
+  // that a machine may
+  // have multiple interfaces and this is a way to select which interface to
+  // listen on.
+  // If a machine has multiple interfaces that require responders then multiple
+  // Responders can be used
+  // to achieve this functonality. If the local ip address/port combination is
+  // invalid or in use then
+  // a SocketException is to be thrown. Any other errors that result in a problem
+  // that would allow
+  // things not to be monitored will result in a FailureDetectorException being
+  // thrown.
 
-    public Responder(int port, InetAddress laddr) throws FailureDetectorException, SocketException {
-	System.out.println("Responder needs to be implemented");
+  public Responder(int port, InetAddress laddr) throws FailureDetectorException, SocketException {
+    this.port = port;
+    this.addr = laddr;
+
+    if (!isPortAndAddressAvailable(port, laddr)) {
+      throw new SocketException("Local IP Address and port is in use already.");
     }
 
-    // Prior to this method being invoked all heartbeat messages are ignored/discarded.
-    // If startResponding() is invoked on an instance that is already responding then
-    // a FailureDectectorException with the message "Already Running" is to be thrown.
-    // If any other problem is detected then the FailureDectorException is thrown
-    public void startResponding()throws FailureDetectorException {
-	System.out.println("startResponding needs to be implemented");
+    if (!isPortAvailable(port, laddr)) {
+      throw new SocketException("Port is not available.");
     }
 
-    // Once this method returns heartbeat messages will be discraded/ignored until a subsequent
-    // startResponding call is made.
+    var map = new HashMap<Integer, Boolean>();
+    map.put(port, false);
+    responders.put(addr, map);
+  }
 
-    public void stopResponding() {
-	System.out.println("stopResponding needs to be implemented");
+  @Override
+  public void run() {
+    while (this.isResponding) {
+      byte[] buf = new byte[1024];
+      DatagramPacket packet = new DatagramPacket(buf, buf.length);
+
+      this.socket = new DatagramSocket(this.port, this.addr);
+      this.socket.receive(packet);
+
+      ByteBuffer response = ByteBuffer.wrap(packet.getData());
+
+      if (response.capacity() == MESSAGE_SIZE_IN_BYTES) {
+        long epochNonce = response.getLong();
+        long seqNumber = response.getLong();
+        InetAddress responseAddress = packet.getAddress();
+        int responsePort = packet.getPort();
+
+        this.sendAck(epochNonce, seqNumber, responsePort, responseAddress);
+      }
     }
+  }
+
+  boolean isPortAndAddressAvailable(int port, InetAddress address) {
+    if (responders.containsKey(address)) {
+      var map = responders.get(address);
+      return !map.containsKey(port);
+    }
+    return true;
+  }
+
+  boolean isResponding(int port, InetAddress address) {
+    if (responders.containsKey(address)) {
+      var map = responders.get(address);
+      return map.get(port) && this.isResponding;
+    }
+    return false;
+  }
+
+  public static boolean isPortAvailable(int port, InetAddress addr) {
+    try (var ds = new DatagramSocket(port, addr)) {
+      return true;
+    } catch (IOException e) {
+      return false;
+    }
+  }
+
+  private void sendAck(long epochNonce, long seqNumber, int port, InetAddress address) {
+    final ByteBuffer buffer = ByteBuffer.allocate(MESSAGE_SIZE_IN_BYTES);
+    buffer.putLong(epochNonce);
+    buffer.putLong(seqNumber);
+    buffer.rewind();
+    DatagramPacket ack = new DatagramPacket(buffer.array(), buffer.limit(), address, port);
+
+    socket.send(ack);
+  }
+
+  // Prior to this method being invoked all heartbeat messages are
+  // ignored/discarded.
+  // If startResponding() is invoked on an instance that is already responding
+  // then
+  // a FailureDectectorException with the message "Already Running" is to be
+  // thrown.
+  // If any other problem is detected then the FailureDectorException is thrown
+  public void startResponding() throws FailureDetectorException, SocketException {
+    if (isResponding(this.port, this.addr)) {
+      throw new FailureDetectorException("Already running.");
+    } else {
+      responders.get(this.addr).replace(this.port, true);
+    }
+    this.isResponding = true;
+    this.start();
+  }
+
+  // Once this method returns heartbeat messages will be discraded/ignored until a
+  // subsequent
+  // startResponding call is made.
+  public void stopResponding() {
+    this.isResponding = false;
+  }
 }
-    
